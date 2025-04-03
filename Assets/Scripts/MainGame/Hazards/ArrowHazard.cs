550e546c9854124f141493e0928bca0f041a76ce
droritos <drorgu102@gmail.com>
2025-04-03 21:07:51 +0300

    Fixed

--- Assets/Scripts/MainGame/Hazards/ArrowHazard.cs
+++ Assets/Scripts/MainGame/Hazards/ArrowHazard.cs
@@ -7,24 +7,17 @@ public class ArrowHazard : MonoBehaviour
     [SerializeField] float shootInterval;
     private float shootIntervalLeft;
     
-    private void Awake()
-    {
-        
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         shootIntervalLeft = shootInterval;
     }
 
-    // Update is called once per frame
     void Update()
     {
         shootIntervalLeft -= Time.deltaTime;
         if (shootIntervalLeft <= 0)
         {
-            ArrowObject arrow = Instantiate(arrowPrefab,transform.position,Quaternion.identity).GetComponent<ArrowObject>();
+            GameObject arrow = Instantiate(arrowPrefab, transform.position, Quaternion.identity);
             arrow.transform.Rotate(0,90,0);
             arrow.transform.Rotate(0,90,0);
             shootIntervalLeft = shootInterval;
